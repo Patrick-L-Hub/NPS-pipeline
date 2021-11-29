9d415c120f551e7477907a872372e6ff6b324560
Patrick LaBarre <pglabarre@gmail.com>
2021-11-29 16:15:12 -0600

    Update nps_api_scrape.py

--- scripts/nps_api_scrape.py
+++ scripts/nps_api_scrape.py
@@ -7,10 +7,18 @@ import requests
 
 
 def makeSoup(webpage):
+    '''
+    create a beautiful soup object that represents the webpage as a nested data structure
+    '''
     soup = BeautifulSoup(webpage.content,"html.parser")
     return soup
 
 def api_scrape():
+    '''
+    submit request to nps.gov api sequentially to grab all data listed ont the
+    nps.gov documentation webpage. webcam data is omitted
+    replace INSERT KEY HERE with your own personal access key 
+    '''
     key = 'INSERT KEY HERE'
     nps_contents = {}
     content = ['activities','alerts','amenities','articles','campgrounds','events','lessonplans','newsreleases','parks','passportstamplocations','people','places','thingstodo','topics','tours','visitorcenters']
